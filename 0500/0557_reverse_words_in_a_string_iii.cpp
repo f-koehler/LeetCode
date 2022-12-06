589e9d59cb1e5277e3ed954f332af1bf3c4e3b2f
Fabian Köhler <fabian.koehler@protonmail.ch>
2022-12-06 18:50:44 +0100

    Add 0557.

---
+++ 0500/0557_reverse_words_in_a_string_iii.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <catch2/catch_test_macros.hpp>
+#include <string>
+using namespace std;
+
+class Solution {
+  public:
+    string reverseWords(string s) {
+        auto word_start = s.begin();
+        auto word_end = s.begin();
+        while (word_end != s.end()) {
+            ++word_end;
+            if (*word_end == ' ') {
+                std::reverse(word_start, word_end);
+                ++word_end;
+                word_start = word_end;
+            }
+        }
+        if (word_start < word_end) {
+            std::reverse(word_start, word_end);
+        }
+        return s;
+    }
+};
+
+TEST_CASE("0557 - Reverse Words in a String III") {
+    Solution s;
+    REQUIRE(s.reverseWords("Let's take LeetCode contest") ==
+            "s'teL ekat edoCteeL tsetnoc");
+    REQUIRE(s.reverseWords("God Ding") == "doG gniD");
+}
