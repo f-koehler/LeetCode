567f303682fe59d7277b5381386021a1f92c46e8
Fabian Köhler <me@fkoehler.org>
2024-10-04 21:10:13 +0800

    Setup pre-commit hooks.

--- devenv.nix
+++ devenv.nix
@@ -37,7 +37,63 @@
   '';
 
   # https://devenv.sh/pre-commit-hooks/
-  # pre-commit.hooks.shellcheck.enable = true;
+  pre-commit.hooks = {
+    # C++
+    clang-format = {
+      enable = true;
+      types_or = ["c++" "c"];
+    };
+    clang-tidy = {
+      enable = false;
+      types_or = ["c++" "c"];
+      entry = "clang-tidy -p build --fix";
+    };
+    clazy = {
+      enable = false;
+      types = ["c++"];
+      entry = "clazy-standalone -p build/compile_commands.json";
+    };
+    cppcheck = {
+      enable = false;
+      types = ["c++"];
+      entry = ''
+        cppcheck \
+            --check-level=exhaustive \
+            --enable=performance \
+            --enable=portability \
+            --enable=style \
+            --enable=warning \
+            --library=qt \
+            --error-exitcode=1 \
+            --inline-suppr \
+            --suppress=unusedStructMember
+      '';
+    };
+    flawfinder = {
+      enable = false;
+      pass_filenames = false;
+      entry = "flawfinder --error-level=0 ./src";
+    };
+
+    # CMake
+    cmake-format = {
+      enable = true;
+      types = ["cmake"];
+      entry = "cmake-format -c .cmake.yaml -i";
+    };
+    cmake-lint = {
+      enable = true;
+      types = ["cmake"];
+      entry = "cmake-lint -c .cmake.yaml";
+    };
+
+    # nix
+    alejandra.enable = true;
+    deadnix.enable = true;
+    flake-checker.enable = true;
+    nil.enable = true;
+    statix.enable = true;
+  };
 
   # See full reference at https://devenv.sh/reference/options/
 }
