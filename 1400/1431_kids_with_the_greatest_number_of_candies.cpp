37b084a244724179e5acc17b922805e32cf476bd
Fabian Köhler <me@fkoehler.org>
2024-10-09 00:59:29 +0800

    Add problem 1431.

---
+++ 1400/1431_kids_with_the_greatest_number_of_candies.cpp
@@ -0,0 +1,37 @@
+#include <algorithm>
+#include <catch2/catch_test_macros.hpp>
+#include <vector>
+
+class Solution {
+  public:
+    std::vector<bool> kidsWithCandies(const std::vector<int> &candies,
+                                      int extraCandies) {
+        // we can safely dereference as the candies array is not empty
+        const auto max_candies =
+            *std::max_element(candies.cbegin(), candies.cend());
+
+        std::vector<bool> result(candies.size(), false);
+
+        std::transform(candies.cbegin(), candies.cend(), result.begin(),
+                       [extraCandies, max_candies](int candies) {
+                           return candies + extraCandies >= max_candies;
+                       });
+        return result;
+    }
+};
+
+TEST_CASE("1431. Kids With the Greatest Number of Candies", "[Array]") {
+    Solution s;
+    SECTION("Example 1") {
+        REQUIRE(s.kidsWithCandies(std::vector<int>{2, 3, 5, 1, 3}, 3) ==
+                std::vector<bool>{true, true, true, false, true});
+    }
+    SECTION("Example 2") {
+        REQUIRE(s.kidsWithCandies(std::vector<int>{4, 2, 1, 1, 2}, 1) ==
+                std::vector<bool>{true, false, false, false, false});
+    }
+    SECTION("Example 3") {
+        REQUIRE(s.kidsWithCandies(std::vector<int>{12, 1, 12}, 10) ==
+                std::vector<bool>{true, false, true});
+    }
+}
