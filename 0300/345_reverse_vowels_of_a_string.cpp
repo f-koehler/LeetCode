3725a8ab2c0325ab7ef9e7abd2c98fcfb16a0269
Fabian Köhler <me@fkoehler.org>
2024-10-09 12:45:12 +0800

    Add problem 0345.

---
+++ 0300/345_reverse_vowels_of_a_string.cpp
@@ -0,0 +1,57 @@
+#include <catch2/catch_test_macros.hpp>
+#include <iterator>
+#include <string>
+
+inline bool is_vowel(char c) {
+    switch (c) {
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+    case 'A':
+    case 'E':
+    case 'I':
+    case 'O':
+    case 'U':
+        return true;
+    default:
+        return false;
+    }
+}
+
+class Solution {
+  public:
+    std::string reverseVowels(const std::string &s) {
+        std::string result(s);
+        auto left = result.begin();
+        auto right = std::prev(result.end());
+        bool left_is_vowel = is_vowel(*left);
+        bool right_is_vowel = is_vowel(*right);
+        while (left < right) {
+            if (!left_is_vowel) {
+                ++left;
+                left_is_vowel = is_vowel(*left);
+                continue;
+            }
+            if (!right_is_vowel) {
+                --right;
+                right_is_vowel = is_vowel(*right);
+                continue;
+            }
+
+            std::swap(*left, *right);
+            ++left;
+            --right;
+            left_is_vowel = is_vowel(*left);
+            right_is_vowel = is_vowel(*right);
+        }
+        return result;
+    }
+};
+
+TEST_CASE("345 - Reverse Vowels of a String", "[Two Pointers][String]") {
+    Solution s;
+    SECTION("Example 1") { REQUIRE(s.reverseVowels("IceCreAm") == "AceCreIm"); }
+    SECTION("Example 2") { REQUIRE(s.reverseVowels("leetcode") == "leotcede"); }
+}
