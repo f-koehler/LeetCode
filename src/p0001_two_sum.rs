c0fb111faa47616099d17c7c406211ca9b4f4da8
Fabian Koehler <fabian.koehler@proton.me>
2025-03-08 16:30:52 +0800

    Add rust solution for p0001.

---
+++ src/p0001_two_sum.rs
@@ -0,0 +1,26 @@
+struct Solution {}
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut map = std::collections::HashMap::new();
+        for (i, &num) in nums.iter().enumerate() {
+            if let Some(&j) = map.get(&(target - num)) {
+                return vec![j as i32, i as i32];
+            }
+            map.insert(num, i);
+        }
+        unreachable!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::p0001_two_sum::Solution;
+
+    #[test]
+    fn test_two_sum() {
+        assert_eq!(Solution::two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
+        assert_eq!(Solution::two_sum(vec![3, 2, 4], 6), vec![1, 2]);
+        assert_eq!(Solution::two_sum(vec![3, 3], 6), vec![0, 1]);
+    }
+}
